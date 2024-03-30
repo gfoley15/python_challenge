fa2db47a07776ba3f6f55dd21a476bacc1634357
gfoley15 <garrett.foley15@gmail.com>
2024-03-30 12:08:22 -0500

    completed find requirements

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,16 +1,16 @@
-#The total number of votes cast
-#A complete list of candidates who received votes
-#The percentage of votes each candidate won
-#The total number of votes each candidate won
-#The winner of the election based on popular vote
+# The total number of votes cast
+# A complete list of candidates who received votes
+# The percentage of votes each candidate won
+# The total number of votes each candidate won
+# The winner of the election based on popular vote
 
-#print the analysis to the terminal and export a text file with the results
-#Election Results
-#Total Votes: 369711
-#Charles Casper Stockham: 23.049% (85213)
-#Diana DeGette: 73.812% (272892)
-#Raymon Anthony Doane: 3.139% (11606)
-#Winner: Diana DeGette
+# print the analysis to the terminal and export a text file with the results
+# Election Results
+# Total Votes: 369711
+# Charles Casper Stockham: 23.049% (85213)
+# Diana DeGette: 73.812% (272892)
+# Raymon Anthony Doane: 3.139% (11606)
+# Winner: Diana DeGette
 
 ### start here, import election data
 
@@ -20,16 +20,44 @@ import csv
 file_path = "PyPoll/Resources/election_data.csv"
 vote_count = 0
 candidates = []
-
+candidate_votes = []
 # open election file
 with open(file_path) as election_data:
     csv_file = csv.reader(election_data)
-    next(csv_file)
+    next(csv_file) # skips a row in the file, the header row
+    # read a row in the file
     for row in csv_file:
-        vote_count += 1
+        vote_count += 1 # add to total votes
+        # establish index for ballot id, county, candidate
+        ballot_id = row[0]
+        county = row[1]
+        candidate = row[2]
+        # check to see if the candidate exists
+        if candidate not in candidates:
+            candidates.append(candidate) # add candidate to the list if they are not in the list
+            candidate_votes.append(1) # add the first vote
+        else: #candidate is in the list
+            candidate_id = candidates.index(candidate) # establish index for candidate
+            candidate_votes[candidate_id] += 1 # add the next votes
+# done reading the file
+# find the winner
+winner = ""
+winner_vote_count = 0
+for candidate in candidates:
+    current_winning_votes = candidate_votes[candidates.index(candidate)]
+    if current_winning_votes > winner_vote_count:
+            winner = candidate
+            winner_vote_count = current_winning_votes
 
-#print the results
+# print the results to the screen
 print("Election Results")
 print('----------------------------')
 print(f'Total Votes: {vote_count}')
+print('----------------------------')
+for candidate in candidates:
+    current_candidate_votes = candidate_votes[candidates.index(candidate)] # varible inside for loop for unique candidate vote count
+    vote_percentage = round((current_candidate_votes / vote_count)*100, 3) # variable inside for loop for calculating percent of vote for each candiate
+    print(f'{candidate}: {vote_percentage}% ({current_candidate_votes})')
+print('----------------------------')
+print(f'Winner: {winner}')
 print('----------------------------')

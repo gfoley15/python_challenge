350f778134a3d27b3a19f0650d387be138c0b95c
gfoley15 <garrett.foley15@gmail.com>
2024-03-30 10:48:00 -0500

    created files, added instructions

---
+++ PyBank/main.py
@@ -0,0 +1,13 @@
+#The total number of months included in the dataset
+#The net total amount of "Profit/Losses" over the entire period
+#The changes in "Profit/Losses" over the entire period, and then the average of those changes
+#The greatest increase in profits (date and amount) over the entire period
+#The greatest decrease in profits (date and amount) over the entire period
+
+#both print the analysis to the terminal and export a text file with the results
+#Financial Analysis
+#Total Months: 86
+#Total: $22564198
+#Average Change: $-8311.11
+#Greatest Increase in Profits: Aug-16 ($1862002)
+#Greatest Decrease in Profits: Feb-14 ($-1825558)

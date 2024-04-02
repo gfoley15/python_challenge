a5c1698cdd155d56ce6f154a05271a6447060ad0
gfoley15 <garrett.foley15@gmail.com>
2024-04-02 11:38:15 -0500

    removed extra comments

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,17 +1,3 @@
-# The total number of votes cast
-# A complete list of candidates who received votes
-# The percentage of votes each candidate won
-# The total number of votes each candidate won
-# The winner of the election based on popular vote
-
-# print the analysis to the terminal and export a text file with the results
-# Election Results
-# Total Votes: 369711
-# Charles Casper Stockham: 23.049% (85213)
-# Diana DeGette: 73.812% (272892)
-# Raymon Anthony Doane: 3.139% (11606)
-# Winner: Diana DeGette
-
 ### start here, import election data
 
 import csv
@@ -44,16 +30,6 @@ with open(file_path, 'r') as election_data:
 
 # find the winner
 winner = candidates[candidate_votes.index(max(candidate_votes))]
-
-    # brute force version for finding winner, can be ignored and maintaining as reference
-    # print(max(candidate_votes))
-    # print(candidates[candidate_votes.index(max(candidate_votes))])# winner = ""
-    # winner_vote_count = 0
-    # for candidate in candidates:
-    #    current_winning_votes = candidate_votes[candidates.index(candidate)]
-    #    if current_winning_votes > winner_vote_count:
-    #            winner = candidate
-    #            winner_vote_count = current_winning_votes
 
 # print the results to the screen
 print("Election Results")
